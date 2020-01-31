8e95ca95d4c693cd79952a5ff30b3d579b4abdcf
djole <dzordzcook@gmail.com>
2020-01-31 19:05:46 +0100

    Implemented custom scopes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+3.times do |topic|
+ Topic.create!(
+  title: "Topic #{topic}"
+  )
+end
+
+puts "3 topics created!"
+
 10.times do |blog|
   Blog.create!(
       title: "My Blog Post #{blog}",
@@ -12,7 +20,8 @@
         in the 1960s with the release of Letraset sheets 
         containing Lorem Ipsum passages, and more recently 
         with desktop publishing software like Aldus PageMaker
-         including versions of Lorem Ipsum."
+         including versions of Lorem Ipsum.",
+         topic_id: Topic.last.id
   	)
 end
 
@@ -27,10 +36,10 @@ end
 
 puts "5 skills posts created."
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
 	Portfolio.create!(
         title: "Portfolio title: #{portfolio_item}",
-        subtitle: "My greast service.",
+        subtitle: "Ruby on Rails.",
         body:"It is a long established fact that a reader will be 
         distracted by the readable content of a page when looking 
         at its layout. The point of using Lorem Ipsum is that it
@@ -41,4 +50,20 @@ puts "5 skills posts created."
         thumb_image:"http://placehold.it/350x200"
         )
 end
+
+1.times do |portfolio_item|
+  Portfolio.create!(
+        title: "Portfolio title: #{portfolio_item}",
+        subtitle: "Angular.",
+        body:"It is a long established fact that a reader will be 
+        distracted by the readable content of a page when looking 
+        at its layout. The point of using Lorem Ipsum is that it
+         has a more-or-less normal distribution of letters, as 
+         opposed to using 'Content here, content here', making it 
+         look like readable English.",
+        main_image: "http://placehold.it/600x400",
+        thumb_image:"http://placehold.it/350x200"
+        )
+end
+
 puts "9 portfolio items created."
