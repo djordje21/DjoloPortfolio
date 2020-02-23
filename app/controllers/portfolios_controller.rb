966241fadc7bb9e10056bc135df57669a3268554
djole <dzordzcook@gmail.com>
2020-02-23 21:27:35 +0100

    Fixes

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -26,11 +26,13 @@ class PortfoliosController < ApplicationController
      
      def update
      		 @portfolio_item = Portfolio.find(params[:id])
+
+
     respond_to do |format|
-      if @portfolio_item.update(portfolio_params)
-        format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
-      else
-        format.html { render :edit }
+          if @portfolio_item.update(portfolio_params)
+            format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+          else
+            format.html { render :edit }
       end
     end
   end
